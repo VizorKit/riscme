2611bf2d6051609a23307e5ba9ca74e8e2109d13
VizorKit <christopher.clark@vizorkit.com>
2021-01-05 21:48:33 -0600

    need to find out how to write to memory more generically

--- os/src/addresses.rs
+++ os/src/addresses.rs
@@ -1,10 +1,10 @@
 #[derive(Clone, Copy)]
-pub struct PhysAddr(u32);
+pub struct PhysAddr(usize);
 #[derive(Clone, Copy)]
-pub struct Offset(u32);
+pub struct Offset(usize);
 
 impl PhysAddr {
-    pub const fn new(addr: u32) -> Self {
+    pub const fn new(addr: usize) -> Self {
         PhysAddr(addr)
     }
 
@@ -12,13 +12,22 @@ impl PhysAddr {
         PhysAddr(self.0 + offset.0)
     }
 
-    pub const fn value(self) -> u32 {
+    pub const fn value(self) -> usize {
         self.0
+    }
+    pub fn write_to(self, set_to: usize) -> () {
+        unsafe {
+            let ptr = self.value() as *mut usize;
+            *ptr = set_to;
+        }
+    }
+    pub fn read_from(self) -> usize {
+        unsafe { *(self.value() as *const usize) }
     }
 }
 
 impl Offset {
-    pub const fn new(offset: u32) -> Self {
+    pub const fn new(offset: usize) -> Self {
         Offset(offset)
     }
 }
--- os/src/plic.rs
+++ os/src/plic.rs
@@ -1,4 +1,4 @@
-use crate::addresses::{Offset, PhysAddr};
+use crate::addresses::PhysAddr;
 
 const PLIC_ORIGIN: PhysAddr = PhysAddr::new(0x0C00_0000);
 const PLIC_PENDING1: PhysAddr = PhysAddr::new(0x0C001_0000);
@@ -6,9 +6,8 @@ const PLIC_PENDING2: PhysAddr = PhysAddr::new(0x0C001_0004);
 const PLIC_ENABLE1: PhysAddr = PhysAddr::new(0x0C00_2000);
 const PLIC_ENABLE2: PhysAddr = PhysAddr::new(0x0C00_2004);
 const PLIC_THRESHOLD: PhysAddr = PhysAddr::new(0x0C20_0000);
-const AON_WATCHDOG: Offset = Offset::new(0x01);
+
 pub enum Threshold {
-    Zero = 0b000,
     One = 0b001,
     Two = 0b010,
     Three = 0b011,
@@ -17,7 +16,6 @@ pub enum Threshold {
     Six = 0b110,
     Seven = 0b111,
 }
-
-pub unsafe fn set_threshold(threshold: Threshold) -> () {
-    let current = *(PLIC_THRESHOLD.value() as *mut u32);
+pub fn set_threshold(threshold: Threshold) -> () {
+    PLIC_THRESHOLD.write_to(threshold as usize);
 }
