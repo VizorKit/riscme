26c6db3e582fc47c4d07d72601772fe06a7bd7c7
VizorKit <christopher.clark@vizorkit.com>
2021-01-14 23:01:27 -0600

    super gross, undoing

--- os/src/direct.rs
+++ os/src/direct.rs
@@ -1,66 +1,56 @@
-use crate::addresses::PhysAddress;
 use core::cell::UnsafeCell;
-use core::ptr::{read_volatile, write_volatile};
+use core::marker::PhantomData;
+use core::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, Not};
 
 #[repr(transparent)]
-pub struct Volatile<T>(UnsafeCell<T>);
-#[repr(transparent)]
-pub struct Maskable<U>(Volatile<U>);
-unsafe impl<T> Sync for Volatile<T> {}
-unsafe impl<T> Send for Volatile<T> {}
+pub struct Volatile<T, V>(UnsafeCell<T>, PhantomData<V>);
+pub struct Mask<T, U, V>(Volatile<T, V>, PhantomData<U>);
 
-impl<T: Copy + 'static> Volatile<T> {
+impl<T: Copy + 'static, U: Copy + 'static> Volatile<T, U> {
     pub fn read_from(&self) -> T {
-        unsafe {
-            let ptr = &self.0.get();
-            read_volatile(*ptr)
-        }
+        unsafe { self.0.get().read_volatile() }
     }
-    pub fn write_to(&self, val: T) {
+    pub fn write_to(&self, val: U) {
         unsafe {
-            let ptr = &self.0.get();
-            write_volatile(*ptr, val)
+            let ptr = self.0.get().write_volatile(val);
         }
     }
 }
 
-pub fn mask_to(&self, val: T, mask: usize, shift: usize) {
-    unsafe {
-        let ptr = &self.0.get();
-        let mut copy = read_volatile(*ptr);
-        copy &= mask;
-        write_volatile(*ptr, copy | (val & (!mask)) << shift);
+impl<
+        T: BitOrAssign<U>
+            + BitOrAssign<T>
+            + BitOrAssign<V>
+            + BitAndAssign<U>
+            + BitAndAssign<V>
+            + BitAndAssign<T>
+            + BitOr
+            + BitAnd
+            + Not
+            + Copy
+            + 'static,
+        U: BitOrAssign + BitAndAssign + BitOr + BitAnd + Not + Copy + 'static,
+        V: BitOrAssign + BitAndAssign + BitOr + BitAnd + Not + Copy + 'static,
+    > Mask<T, U, V>
+{
+    pub fn mask_to(self, mask: U, mut value: V) {
+        unsafe {
+            let mut copy = self.0.read_from();
+            copy &= mask;
+            value &= !mask;
+            self.0.write_to(copy | value)
+        }
     }
 }
-pub fn or_to(&self, val: T) {
-    unsafe {
-        let ptr = &self.0.get();
-        write_volatile(*ptr, read_volatile(*ptr) | val)
+impl<
+        T: BitOrAssign<V> + BitOrAssign<T> + BitAndAssign + BitOr + BitAnd + Not + Copy + 'static,
+        U: BitOrAssign + BitAndAssign + BitOr + BitAnd + Not + Copy + 'static,
+        V: BitOrAssign + BitAndAssign + BitOr + BitAnd + Not + Copy + 'static,
+    > Mask<T, U, V>
+{
+    pub fn or_to(self, value: V) {
+        let mut copy: T = self.0.read_from();
+        copy |= value;
+        self.0.write_to(copy);
     }
-}
-
-pub fn set_to(item: PhysAddress, set_val: usize, shift_val: usize) {
-    unsafe {
-        let ptr = item.value() as *mut usize;
-        write_volatile(ptr, set_val << shift_val);
-    }
-}
-
-pub fn mask_to(item: PhysAddress, mask_val: usize, set_val: usize, shift_val: usize) {
-    unsafe {
-        let ptr = item.value() as *mut usize;
-        let mut copy = read_volatile(ptr);
-        copy &= mask_val;
-        write_volatile(ptr, copy | (set_val & (!mask_val)) << shift_val);
-    }
-}
-pub fn or_to(item: PhysAddress, or_val: usize, shift_val: usize) {
-    unsafe {
-        let ptr = item.value() as *mut usize;
-        write_volatile(ptr, read_volatile(ptr) | or_val << shift_val);
-    }
-}
-
-fn read_from(item: PhysAddress) -> usize {
-    unsafe { read_volatile(item.value() as *const usize) }
 }
